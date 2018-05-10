c30cb7388b0030f98ef6efd0abcb412ab90532b9
Hugo <hugo.polux@gmail.com>
2018-05-10 19:38:25 +0200

    Maquette v1.2 Affichage et Editeur de la liste de Scenario

--- app/src/main/java/com/projetRobot/CreateScenarioActivity.java
+++ app/src/main/java/com/projetRobot/CreateScenarioActivity.java
@@ -2,13 +2,23 @@ package com.projetRobot;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.projetRobot.Scenario;
 
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+
+
 
 /**
  * Created by Hugo on 08/05/2018.
@@ -22,25 +32,56 @@ public class CreateScenarioActivity extends Activity {
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-        ArrayList<Scenario> listScenario = new ArrayList<Scenario>();
 
-        scenario =  findViewById(R.id.newScenario);
-        
+
+
+
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.createscenario_activity);
+        scenario = (EditText) findViewById(R.id.newScenario);
         context = this.getApplicationContext();
         validerScenario=findViewById(R.id.validerScenario);
         validerScenario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            String nomScenario=scenario.getText().toString();
-            Scenario scenarioCree = new Scenario(nomScenario);
-            ListScenarioActivity.listScenario.add(scenarioCree);
+                ArrayList<Scenario> listScenToUpdate =new ArrayList<Scenario>();
 
+                Scenario scenarioObject=new Scenario();
+                scenarioObject.setName(scenario.getText().toString());
+
+
+                SharedPreferences sharedPreferences=getSharedPreferences("shared preferences",MODE_PRIVATE);
+
+
+                Gson gson= new Gson();
+                String json=sharedPreferences.getString("testlist",null);
+                Type type =new TypeToken<ArrayList<Scenario>>(){}.getType();
+                System.out.println("JSON"+json);
+
+                listScenToUpdate= gson.fromJson(json, type);
+
+                if ( listScenToUpdate==null){
+                    listScenToUpdate= new ArrayList<>();
+                }
+                listScenToUpdate.add(scenarioObject);
+
+
+
+
+
+
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                Gson gsonpush=new Gson();
+                String jsonpush=gsonpush.toJson(listScenToUpdate);
+                editor.putString("testlist",jsonpush);
+                editor.apply();
             }
         });
     }
+
+
+
 }
 
--- app/src/main/java/com/projetRobot/LauncherActivity.java
+++ app/src/main/java/com/projetRobot/LauncherActivity.java
@@ -3,9 +3,18 @@ package com.projetRobot;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by Hugo on 27/04/2018.
@@ -15,21 +24,21 @@ public class LauncherActivity  extends Activity {
     private Button addInter, editInter, addScenario;
     private Button listeScenario;
     private Context context;
+    ListView listView ;
+    public  ArrayList<Scenario> getListScenario() {
+        return listScenario;
+    }
 
+     ArrayList<Scenario> listScenario;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         context = this.getApplicationContext();
 
-        addInter=findViewById(R.id.addInterBtn);
-        addInter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(context, AddActionActivity.class);
-                startActivity(myIntent);
-            }
-        });
+
 
         listeScenario=findViewById(R.id.listeScenarioBtn);
         listeScenario.setOnClickListener(new View.OnClickListener() {
@@ -49,7 +58,9 @@ public class LauncherActivity  extends Activity {
             }
         });
 
+
     }
+
 }
 
 
